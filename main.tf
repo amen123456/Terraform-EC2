8e9b822de0291c549f916c55094742f24accba71
amen123456 <61538514+amen123456@users.noreply.github.com>
2024-05-26 14:59:03 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,49 +3,41 @@ resource "tls_private_key" "rsa_4096" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
-
 variable "key_name" {
   description = "Name of the SSH key pair"
 }
-
 // Create Key Pair for Connecting EC2 via SSH
 resource "aws_key_pair" "key_pair" {
   key_name   = var.key_name
   public_key = tls_private_key.rsa_4096.public_key_openssh
 }
-
 // Save PEM file locally
 resource "local_file" "private_key" {
   content  = tls_private_key.rsa_4096.private_key_pem
   filename = "${path.module}/${var.key_name}.pem"
 }
-
 // Create a security group
 resource "aws_security_group" "sg_ec2" {
   name        = "sg_ec2"
   description = "Security group for EC2"
-
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 27017
     to_port     = 27017
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
